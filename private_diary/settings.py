1741419feab4354a532c7c690f0c259797f68173
kanyamo <kanajir@yahoo.co.jp>
2022-06-22 00:31:31 +0900

    indexの作成

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -121,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'  # 各アプリケーションごとに
+
+STATICFILES_DIRS = [  # プロジェクト全体に関する静的ファイル
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
